686afb01b55f653b30eaacc3c1f2753b65903b81
framadhan99 <fajarramadhan99704@gmail.com>
2023-03-12 15:55:18 +0700

    Intro Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe_game/pages/home_page.dart';
+import 'package:tic_tac_toe_game/pages/intro_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: IntroPage(),
     );
   }
 }
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tic_tac_toe_game/pages/home_page.dart';
 
 import '../utils/styles.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  void initState() {
+    super.initState();
+    nextPage();
+  }
+
+  Future nextPage() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +38,7 @@ class IntroPage extends StatelessWidget {
       body: Center(
           child: Text(
         'Tic-Tac-Toe Game',
-        style: Styles.textPriamry,
+        style: Styles.textPriamry.copyWith(fontSize: 32),
       )),
     );
   }
